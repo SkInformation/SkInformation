6b40598af84977b28339c9ce4897d6e7f24b6b92
Alisha <56981295+alishcodes@users.noreply.github.com>
2023-10-20 15:31:15 -0700

    Created IngredientAttribute model

---
+++ Backend/Backend-Models/Models/IngredientAttribute.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Models.Models
+{
+    public class IngredientAttribute
+    {
+        [Key]
+        public string Name { get; set; }
+        public string Usage { get; set; }   //Ex: Glycerin improves hydration.
+        public bool EyeIrritant { get; set; }
+        public bool DriesSkin { get; set; }
+        public bool ReducesRedness { get; set; }
+        public bool Hydrating { get; set; }
+        public bool NonComedogenic { get; set; }
+        public bool SafeForPregnancy { get; set; }
+    }
+}
