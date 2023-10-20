bf0b35788ca87d69b6a36376f9f0f25f23b582cc
Alisha <56981295+alishcodes@users.noreply.github.com>
2023-10-20 15:35:14 -0700

    Added migration for IngredientAttribute table

--- Backend/Backend/Database/AppDbContext.cs
+++ Backend/Backend/Database/AppDbContext.cs
@@ -7,6 +7,7 @@ namespace Backend
     {
         public DbSet<Product> Products { get; set; }
         public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<IngredientAttribute> IngredientAttributes { get; set; }
         
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
