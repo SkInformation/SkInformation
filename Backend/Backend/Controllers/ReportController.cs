4d7b2988e99f5e8989b1cf3af79609aa1e614adb
Alisha <56981295+alishcodes@users.noreply.github.com>
2023-11-07 16:59:26 -0800

    Created GenerateReport endpoint.

--- Backend/Backend/Controllers/ReportController.cs
+++ Backend/Backend/Controllers/ReportController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Backend_Models.Dtos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -17,6 +18,17 @@ namespace Backend.Controllers
         public ReportController(AppDbContext appDbContext)
         {
             _appDbContext = appDbContext;
+        }
+
+        /// <summary>
+        /// Generates a report based on input provided.
+        /// </summary>
+        /// <param name="reportInput">A GenerateReportDto.</param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult GenerateReport(GenerateReportDto reportInput)
+        {
+            return Ok();
         }
     }
 }
