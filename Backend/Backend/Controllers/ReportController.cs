abbe9e6c33900380d50d9f0f79772cd98c738924
Alisha <56981295+alishcodes@users.noreply.github.com>
2023-11-09 17:26:41 -0800

    Created basic email to send in generateReport endpoint.

--- Backend/Backend/Controllers/ReportController.cs
+++ Backend/Backend/Controllers/ReportController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Backend.Services;
 using Backend_Models.Dtos;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -14,10 +16,12 @@ namespace Backend.Controllers
     public class ReportController : Controller
     {
         private readonly AppDbContext _appDbContext;
+        private readonly IEmailService _emailService;
 
-        public ReportController(AppDbContext appDbContext)
+        public ReportController(AppDbContext appDbContext, IEmailService emailService)
         {
             _appDbContext = appDbContext;
+            _emailService = emailService;
         }
 
         /// <summary>
@@ -26,9 +30,24 @@ namespace Backend.Controllers
         /// <param name="reportInput">A GenerateReportDto.</param>
         /// <returns></returns>
         [HttpPost]
-        public IActionResult GenerateReport(GenerateReportDto reportInput)
+        public async Task<IActionResult> GenerateReport(GenerateReportDto reportInput)
         {
-            return Ok();
+            var status = await _emailService
+                .SendEmail(new (){
+                    To = reportInput.Email,
+                    From = "testing@acio.dev",
+                    TrackOpens = true,
+                    Subject = "Your SkInformation Report",
+                    TextBody = "Your report is ready!",
+                    HtmlBody = "<h1>Your routine is great!</h1>",
+                    Tag = "Customized user report about skincare."
+                });
+
+            if (status) {
+                return Ok();
+            }
+            
+            return new StatusCodeResult(StatusCodes.Status500InternalServerError);
         }
     }
 }
