f5bde3fae98e8574389a6b1e1322144e003e441e
chance-schultz <cschultz909@gmail.com>
2023-11-13 20:10:59 -0800

    `Chance` implement survey navigation (#5)

--- Client/src/app/context/SurveyContext.tsx
+++ Client/src/app/context/SurveyContext.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import {createContext, useContext} from "react";
-interface SurveyContextType {
-    currentStep: number;
-    stepConfig: StepConfig[];
+import {createContext, Dispatch, SetStateAction, useContext, useEffect, useState} from "react";
+
+export enum SurveyStep {
+    SkinType, SkinGoals, Products, Analysis
 }
 
 interface StepConfig {
@@ -42,14 +42,87 @@ const useSurvey = (): SurveyContextType => {
     return context;
 };
 
-interface SurveyProviderProps {
+export interface SurveyProviderProps {
     children: React.ReactNode;
 }
 
+export enum ProductReaction
+{
+    Flakiness, Redness, Swelling, Itchiness
+}
+
+export type Product = {
+    id: Number;
+    reactions: ProductReaction[]
+}
+
+export enum SkinGoal
+{
+    ReduceRedness, ReduceWrinkles, PoreAppearance, EvenSkinTone, MoistureRetention, SkinGlow
+}
+
+export enum SkinType
+{
+    Dry, Normal, Combination, Oily
+}
+
+interface SurveyContextType {
+    currentStep: SurveyStep;
+    stepConfig: StepConfig[];
+    navigateForward: () => void,
+    navigateBackward: () => void,
+    skinType?: SkinType,
+    setSkinType: Dispatch<SetStateAction<SkinType | undefined>>,
+    skinGoals: SkinGoal[],
+    setSkinGoals: Dispatch<SetStateAction<SkinGoal[]>>,
+    products: Product[],
+    setProducts: Dispatch<SetStateAction<Product[]>>,
+}
+
 const SurveyProvider: React.FC<SurveyProviderProps> = ({ children }) => {
+    const [ currentStep, setCurrentStep ] = useState<SurveyStep>(SurveyStep.SkinType)
+    const [ skinType, setSkinType ] = useState<SkinType | undefined>()
+    const [ skinGoals, setSkinGoals ] = useState<SkinGoal[]>([])
+    const [ products, setProducts ] = useState<Product[]>([])
+    const navigateForward = (): void => {
+        if (currentStep < stepConfig.length-1) {
+            setCurrentStep(currentStep+1)
+        } else {
+            console.error("Attempting to navigate forward beyond the bounds of step config")
+        }
+    }
+
+    const navigateBackward = (): void => {
+        if (currentStep > 0) {
+            setCurrentStep(currentStep-1)
+        } else {
+            console.error("Attempting to navigate backward beyond the bounds of step config")
+        }
+    }
+
+    useEffect(() => {
+        if (currentStep !== 0) {
+            return;
+        }
+
+        if (!skinType) {
+            setCurrentStep(SurveyStep.SkinType)
+        } else if (skinGoals.length === 0) {
+            setCurrentStep(SurveyStep.SkinGoals)
+        }
+    }, [skinType, skinGoals])
+
     const initialContextValue: SurveyContextType = {
-        currentStep: 1,
-        stepConfig: stepConfig,
+        currentStep,
+        stepConfig,
+        navigateBackward,
+        navigateForward,
+        skinType,
+        setSkinType,
+        skinGoals,
+        setSkinGoals,
+        products,
+        setProducts
     };
 
     return (
--- Client/src/app/survey/Survey.tsx
+++ Client/src/app/survey/Survey.tsx
@@ -1,50 +1,60 @@
 "use client"
 
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Box from "@mui/material/Box";
 import {Step} from "@/app/survey/components/Step";
 import Grid from "@mui/material/Unstable_Grid2";
 import {Button, Typography} from "@mui/material";
-import { useSurvey } from "@/app/context/SurveyContext";
+import {SurveyStep, useSurvey} from "@/app/context/SurveyContext";
 
 export default function Survey() {
-    const { currentStep, stepConfig } = useSurvey();
+    const {currentStep, stepConfig, navigateBackward, navigateForward} = useSurvey();
 
-    let hideBackButton = false
-    let hideNextButton = false
-    let backButtonText, nextButtonText
+    const [hideBackButton, setHideBackButton] = useState(false);
+    const [hideNextButton, setHideNextButton] = useState(false);
+    const [backButtonText, setBackButtonText] = useState('');
+    const [nextButtonText, setNextButtonText] = useState('');
+
     useEffect(() => {
         const step = stepConfig[currentStep];
+
         if (step) {
-            hideBackButton = step.hideBackButton;
-            hideNextButton = step.hideNextButton;
-            backButtonText = step.backButtonText;
-            nextButtonText = step.nextButtonText;
+            setHideBackButton(step.hideBackButton);
+            setHideNextButton(step.hideNextButton);
+            setBackButtonText(step.backButtonText ?? '');
+            setNextButtonText(step.nextButtonText ?? '');
         }
     }, [currentStep])
 
     return (
         <>
             <Box maxWidth="lg" width={100}>
-                <Step hidden={ currentStep !== 1 }>Step1</Step>
-                <Step hidden={ currentStep !== 2 }>Step2</Step>
-                <Step hidden={ currentStep !== 3 }>Step3</Step>
-                <Step hidden={ currentStep !== 4 }>Step4</Step>
+                <Step hidden={ currentStep !== SurveyStep.SkinType }>Step1</Step>
+                <Step hidden={ currentStep !== SurveyStep.SkinGoals }>Step2</Step>
+                <Step hidden={ currentStep !== SurveyStep.Products }>Step3</Step>
+                <Step hidden={ currentStep !== SurveyStep.Analysis }>Step4</Step>
             </Box>
-            <Grid container columns={2}>
-                <Grid display="flex" justifyContent="left">
-                    <Button hidden={hideBackButton} >
-                        <Typography>
-                            {backButtonText ?? 'Back'}
-                        </Typography>
-                    </Button>
+            <Grid container columns={2} width={"100%"}>
+                <Grid xs display="flex" justifyContent="left" alignItems="left">
+                    {
+                        hideBackButton ?
+                            <></> :
+                            <Button variant={"contained"} onClick={navigateBackward}>
+                                <Typography>
+                                    {backButtonText || 'Back'}
+                                </Typography>
+                            </Button>
+                    }
                 </Grid>
-                <Grid display="flex" justifyContent="right">
-                    <Button hidden={hideNextButton} >
-                        <Typography>
-                            {nextButtonText ?? 'Next'}
-                        </Typography>
-                    </Button>
+                <Grid xs display="flex" justifyContent="right" alignItems="right">
+                    {hideNextButton ?
+                        <></> :
+                        <Button variant={"contained"} onClick={navigateForward}>
+                            <Typography>
+                                {nextButtonText || 'Next'}
+                            </Typography>
+                        </Button>
+                    }
                 </Grid>
             </Grid>
         </>
