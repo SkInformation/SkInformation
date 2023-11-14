65fd8db212b0164ed7d2ab06bda96b8429799cde
Chance Schultz <cschultz909@gmail.com>
2023-11-13 18:12:53 -0800

    Updating survey context with navigation and survey state

--- Client/src/app/survey/Survey.tsx
+++ Client/src/app/survey/Survey.tsx
@@ -5,7 +5,7 @@ import Box from "@mui/material/Box";
 import {Step} from "@/app/survey/components/Step";
 import Grid from "@mui/material/Unstable_Grid2";
 import {Button, Typography} from "@mui/material";
-import { useSurvey } from "@/app/context/SurveyContext";
+import {SurveyStep, useSurvey} from "@/app/context/SurveyContext";
 
 export default function Survey() {
     const { currentStep, stepConfig } = useSurvey();
@@ -26,10 +26,10 @@ export default function Survey() {
     return (
         <>
             <Box maxWidth="lg" width={100}>
-                <Step hidden={ currentStep !== 1 }>Step1</Step>
-                <Step hidden={ currentStep !== 2 }>Step2</Step>
-                <Step hidden={ currentStep !== 3 }>Step3</Step>
-                <Step hidden={ currentStep !== 4 }>Step4</Step>
+                <Step hidden={ currentStep !== SurveyStep.SkinType }>Step1</Step>
+                <Step hidden={ currentStep !== SurveyStep.SkinGoals }>Step2</Step>
+                <Step hidden={ currentStep !== SurveyStep.Products }>Step3</Step>
+                <Step hidden={ currentStep !== SurveyStep.Analysis }>Step4</Step>
             </Box>
             <Grid container columns={2}>
                 <Grid display="flex" justifyContent="left">
