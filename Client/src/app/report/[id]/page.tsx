87644a33f0b3620393d7b9d6868c63e24d8a356f
Alisha <56981295+alishcodes@users.noreply.github.com>
2023-12-03 12:08:03 -0800

    Merge initial report page design and use new api link. (#15)
    * Created irritants accordion for report analysis
    
    * Created product recommendation display, Distinguish sections
    
    Co-authored-by: alishcodes <56981295+alishcodes@users.noreply.github.com>
    
    * Use new API url
    
    * Update endpoint in ProductsDisplay for img.
    
    * Display message if no ingredients found.
    
    * Add Stack to space elements.
    
    * Remove bottom margin
    
    * Add Stacks for spacing.
    
    * Set item height to match largest item.
    
    ---------
    
    Co-authored-by: Andres <dev+git@andrescruz.me>

--- Client/src/app/report/[id]/page.tsx
+++ Client/src/app/report/[id]/page.tsx
@@ -1,8 +1,7 @@
 import apiRequest, {HttpMethod} from "@/app/lib/api";
-
-interface Data {
-    example: string;
-}
+import IrritantsAccordion from "../IrritantsAccordion";
+import ProductRecommendationDisplay from "../ProductRecommendationDisplay";
+import {Stack} from "@mui/material";
 
 interface ReportProps {
     params: {
@@ -10,15 +9,58 @@ interface ReportProps {
     }
 }
 
+// Data models for request
+
+export interface ReportDetails {
+    productRecommendations: ProductRecommendation;
+    irritantAnalysis: IrritantAnalysis[];
+}
+
+export interface ProductRecommendation {
+    MOISTURIZER?: Product[];
+    SERUM?: Product[];
+    CLEANSER?: Product[];
+    SUNSCREEN?: Product[];
+}
+
+export interface Product {
+    id: number
+    name: string;
+    description: string;
+    type: string;
+    url: string;
+}
+
+export interface PotentialIrritant {
+    type: string;
+    ingredients: [Ingredient];
+}
+
+export interface Ingredient {
+    id: number
+    name: string;
+    usage: string;
+}
+
+export interface IrritantAnalysis {
+    product: Product;
+    potentialIrritants: [PotentialIrritant];
+}
+
 export default async function Report({params}: ReportProps) {
-    const result = await apiRequest<Data>(HttpMethod.GET, `report/${params.id}`, {}, {}, {}, (err) => {
+    const analysis = await apiRequest<ReportDetails>(HttpMethod.GET, `https://api.skinformation.site/Report/Details`, { reportId: params.id }, undefined, {}, (err) => {
         console.error(err.message)
     });
 
-    // Render your component using the data state
-    // ...
-
     return (
-        <>{JSON.stringify(result, null, 2)}</>
+        <Stack spacing={2} marginBottom={5}>
+            <h1>Recommendations</h1>
+            <ProductRecommendationDisplay prodRecs={analysis.productRecommendations}/>
+            <br/>
+            <h1>Irritants</h1>
+            {analysis.irritantAnalysis.map((ia: IrritantAnalysis) => (
+                <IrritantsAccordion key={ia.product.id} product={ia.product} potentialIrritants={ia.potentialIrritants}/>
+            ))}
+        </Stack>
     );
 }
