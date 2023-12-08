986d90bf243caff5f81f555cf87a212c903ba64f
Eric Yamabe <ericyamabe@Erics-MacBook-Pro-M1-Max-2.local>
2023-12-08 12:01:27 -0800

    undoing changes

--- Client/src/app/layout.tsx
+++ Client/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import './globals.css'
-import { SurveyProvider } from "@/app/context/SurveyContext";
+import {SurveyProvider} from "@/app/context/SurveyContext";
 
 const inter = Inter({ subsets: ['latin'] })
 
