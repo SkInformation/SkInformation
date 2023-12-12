c72e22723b5b04c677152f89ad96ae2548d25e8b
Andres <mail@acio.dev>
2023-12-12 13:19:39 -0800

    Use consistent font throughout various header tags

--- Client/src/app/report/[id]/page.tsx
+++ Client/src/app/report/[id]/page.tsx
@@ -1,7 +1,8 @@
 import apiRequest, {HttpMethod} from "@/app/lib/api";
 import IrritantsAccordion, {IrritantAnalysis} from "./components/IrritantsAccordion";
 import ProductRecommendationDisplay, {ProductRecommendation} from "./components/ProductRecommendationDisplay";
-import {Stack} from "@mui/material";
+import {Stack, Typography} from "@mui/material";
+import styles from '@/app/survey/page.module.css';
 
 interface ReportProps {
     params: {
@@ -20,10 +21,10 @@ export default async function Report({params}: ReportProps) {
 
     return (
         <Stack spacing={2} marginBottom={5}>
-            <h1>Recommendations</h1>
+            <h1 className={styles.titleText}>Recommendations</h1>
             <ProductRecommendationDisplay prodRecs={analysis.productRecommendations}/>
             <br/>
-            <h1>Irritants</h1>
+            <h1 className={styles.titleText}>Irritants</h1>
             {analysis.irritantAnalysis.map((ia: IrritantAnalysis) => (
                 <IrritantsAccordion key={ia.product.id} product={ia.product} potentialIrritants={ia.potentialIrritants}/>
             ))}
